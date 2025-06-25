cb183b19d017bd35e4e5660629db4a9ce2503784
Zain Javed <zain.javed@softvira.com>
2025-06-25 16:13:38 +0500

    Customized shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  return <div>
-    <Button>Home</Button>
-  </div>;
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
+    </div>
+  );
 }
