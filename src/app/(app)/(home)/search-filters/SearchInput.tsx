0494132c3563199e34a872a6ba5fab14a3f5455a
Zain Javed <zain.javed@softvira.com>
2025-07-02 13:34:42 +0500

    Add categories menu on mobile

--- src/app/(app)/(home)/search-filters/SearchInput.tsx
+++ src/app/(app)/(home)/search-filters/SearchInput.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import { Input } from '@/components/ui/input';
-import { SearchIcon } from 'lucide-react';
-import React from 'react';
+import { ListFilterIcon, SearchIcon } from 'lucide-react';
+import React, { useState } from 'react';
+import { CategoriesSidebar } from './CategoriesSidebar';
+import { Button } from '@/components/ui/button';
 
 interface Props {
   disabled?: boolean;
 }
 
 export const SearchInput = ({disabled}: Props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className='flex items-center gap-2 w-full'>
+      <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
       <div className="relative w-full">
         <SearchIcon className='absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500' />
         <Input className='pl-8' placeholder='Search products...' disabled={disabled} />
       </div>
-      {/* TODO: ADD CATEGORIES VIEW ALL BUTTON
-      TODO: ADD LIBRARY BUTTON */}
+       {/* TODO: ADD CATEGORIES VIEW ALL BUTTON */}
+       <Button
+        variant={"elevated"}
+        className='size-12 shrink-0 flex lg:hidden'
+        onClick={() => setIsSidebarOpen(true)}
+       >
+        <ListFilterIcon />
+       </Button>
+
+      {/* TODO: ADD LIBRARY BUTTON  */}
     </div>
   )
 }
